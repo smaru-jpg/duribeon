6b8c3957eaede27a7e5b1a0827b1dfdc1c0794bb
smaru-jpg <k01041993515@gmail.com>
2026-08-27 14:19:21 +0900

    iframe rendering test

--- api/ui-test.js
+++ api/ui-test.js
@@ -1,14 +1,16 @@
 export default function handler(req, res) {
   res.status(200).json({
-    message: "JavaScript 실행 테스트",
+    message: "iframe 렌더링 테스트",
     html: `
       <div>
-        <h2 id="duribeon-test">자바스크립트 실행 전</h2>
+        <h2>두리번 iframe 테스트</h2>
 
-        <script>
-          document.getElementById("duribeon-test").innerText =
-            "✅ 자바스크립트 실행 성공";
-        </script>
+        <iframe
+          src="https://duribeon.vercel.app"
+          width="600"
+          height="400"
+          style="border:2px solid black;"
+        ></iframe>
       </div>
     `
   });
